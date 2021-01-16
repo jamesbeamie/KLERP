ecaf8f11edc511ebb86fa2d8d7c90543ac13c516
James <admin@James.local>
2021-01-17 01:30:01 +0300

    dashboard navigation

--- src/components/farmerDashboard/Profile.js
+++ src/components/farmerDashboard/Profile.js
@@ -1,102 +1,179 @@
-import React from "react";
-import { Modal, Container, Row, Col, Button } from "react-bootstrap";
+// import React from "react";
+// import { Modal, Container, Row, Col, Button } from "react-bootstrap";
 
-const Profile = ({ openProfile }) => {
-  //   const [modalShow, setModalShow] = useState(false);
-  //   useEffect(() => {
-  //     setModalShow(openProfile);
-  //   }, []);
-  function MydModalWithGrid(props) {
-    return (
-      <Modal {...props} aria-labelledby='contained-modal-title-vcenter'>
-        <Modal.Header closeButton>
-          <Modal.Title id='contained-modal-title-vcenter'>Simiyu</Modal.Title>
-        </Modal.Header>
-        <Modal.Body className='show-grid'>
-          <Container>
-            <Row>
-              <Col xs={12} md={4}>
-                Name:
-              </Col>
-              <Col xs={6} md={8}>
-                Simuyu Omondi
-              </Col>
-              <Col xs={12} md={4}>
-                Farmer Id:
-              </Col>
-              <Col xs={6} md={8}>
-                32429
-              </Col>
-              <Col xs={12} md={4}>
-                Residence:
-              </Col>
-              <Col xs={6} md={8}>
-                Naitiri
-              </Col>
-              <Col xs={12} md={4}>
-                Category:
-              </Col>
-              <Col xs={6} md={8}>
-                Outgrower
-              </Col>
-              <Col xs={12} md={4}>
-                Fields:
-              </Col>
-              <Col xs={6} md={8}>
-                5
-              </Col>
-            </Row>
-          </Container>
-        </Modal.Body>
-        <Modal.Body className='show-grid'>
-          <Container>
-            <Row>
-              <Col xs={6} md={4}>
-                Contact:
-              </Col>
-              <Col xs={6} md={4}>
-                Phone No:
-              </Col>
-              <Col xs={6} md={4}>
-                07123456789
-              </Col>
-            </Row>
-            <Row>
-              <Col xs={6} md={4}></Col>
-              <Col xs={6} md={4}>
-                email:
-              </Col>
-              <Col xs={6} md={4}>
-                email@email.com
-              </Col>
-            </Row>
-            <Row>
-              <Col xs={6} md={4}></Col>
-              <Col xs={6} md={4}>
-                Phone No:
-              </Col>
-              <Col xs={6} md={4}>
-                07123456789
-              </Col>
-            </Row>
-          </Container>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={props.onHide}>Close</Button>
-        </Modal.Footer>
-      </Modal>
-    );
-  }
+// const Profile = ({ openProfile }) => {
+//   return (
+//     <React.Fragment>
+//       <Container>
+//         <Row>
+//           <Col xs={12} md={4}>
+//             Name:
+//           </Col>
+//           <Col xs={6} md={8}>
+//             Simuyu Omondi
+//           </Col>
+//           <Col xs={12} md={4}>
+//             Farmer Id:
+//           </Col>
+//           <Col xs={6} md={8}>
+//             32429
+//           </Col>
+//           <Col xs={12} md={4}>
+//             Residence:
+//           </Col>
+//           <Col xs={6} md={8}>
+//             Naitiri
+//           </Col>
+//           <Col xs={12} md={4}>
+//             Category:
+//           </Col>
+//           <Col xs={6} md={8}>
+//             Outgrower
+//           </Col>
+//           <Col xs={12} md={4}>
+//             Fields:
+//           </Col>
+//           <Col xs={6} md={8}>
+//             5
+//           </Col>
+//         </Row>
+//         {/* </Container>
+// </Modal.Body>
+// <Modal.Body className='show-grid'>
+//   <Container> */}
+//         <Row>
+//           <Col xs={6} md={4}>
+//             Contact:
+//           </Col>
+//           <Col xs={6} md={4}>
+//             Phone No:
+//           </Col>
+//           <Col xs={6} md={4}>
+//             07123456789
+//           </Col>
+//         </Row>
+//         <Row>
+//           <Col xs={6} md={4}></Col>
+//           <Col xs={6} md={4}>
+//             email:
+//           </Col>
+//           <Col xs={6} md={4}>
+//             email@email.com
+//           </Col>
+//         </Row>
+//         <Row>
+//           <Col xs={6} md={4}></Col>
+//           <Col xs={6} md={4}>
+//             Phone No:
+//           </Col>
+//           <Col xs={6} md={4}>
+//             07123456789
+//           </Col>
+//         </Row>
+//       </Container>
+//     </React.Fragment>
+//   );
+//   //   const [modalShow, setModalShow] = useState(false);
+//   //   useEffect(() => {
+//   //     setModalShow(openProfile);
+//   //   }, []);
+//   // function MydModalWithGrid(props) {
+//   //   return (
+//   //     <Modal {...props} aria-labelledby='contained-modal-title-vcenter'>
+//   //       <Modal.Header closeButton>
+//   //         <Modal.Title id='contained-modal-title-vcenter'>Simiyu</Modal.Title>
+//   //       </Modal.Header>
+//   //       <Modal.Body className='show-grid'>
+//   //         <Container>
+//   //           <Row>
+//   //             <Col xs={12} md={4}>
+//   //               Name:
+//   //             </Col>
+//   //             <Col xs={6} md={8}>
+//   //               Simuyu Omondi
+//   //             </Col>
+//   //             <Col xs={12} md={4}>
+//   //               Farmer Id:
+//   //             </Col>
+//   //             <Col xs={6} md={8}>
+//   //               32429
+//   //             </Col>
+//   //             <Col xs={12} md={4}>
+//   //               Residence:
+//   //             </Col>
+//   //             <Col xs={6} md={8}>
+//   //               Naitiri
+//   //             </Col>
+//   //             <Col xs={12} md={4}>
+//   //               Category:
+//   //             </Col>
+//   //             <Col xs={6} md={8}>
+//   //               Outgrower
+//   //             </Col>
+//   //             <Col xs={12} md={4}>
+//   //               Fields:
+//   //             </Col>
+//   //             <Col xs={6} md={8}>
+//   //               5
+//   //   </Col>
+//   // </Row>
+//   {
+//     /* </Container>
+//         </Modal.Body>
+//         <Modal.Body className='show-grid'>
+//           <Container> */
+//   }
+//   // <Row>
+//   //   <Col xs={6} md={4}>
+//   //     Contact:
+//   //   </Col>
+//   //   <Col xs={6} md={4}>
+//   //     Phone No:
+//   //   </Col>
+//   //   <Col xs={6} md={4}>
+//   //     07123456789
+//   //   </Col>
+//   // </Row>
+//   // <Row>
+//   //   <Col xs={6} md={4}></Col>
+//   //   <Col xs={6} md={4}>
+//   //     email:
+//   //   </Col>
+//   //   <Col xs={6} md={4}>
+//   //     email@email.com
+//   //   </Col>
+//   // </Row>
+//   // <Row>
+//   //   <Col xs={6} md={4}></Col>
+//   //   <Col xs={6} md={4}>
+//   //     Phone No:
+//   //   </Col>
+//   //   <Col xs={6} md={4}>
+//   //     07123456789
+//   //   </Col>
+//   //           </Row>
+//   //         </Container>
+//   //       </Modal.Body>
+//   //       <Modal.Footer>
+//   //         <Button onClick={props.onHide}>Close</Button>
+//   //       </Modal.Footer>
+//   //     </Modal>
+//   //   );
+//   // }
 
-  return (
-    <React.Fragment>
-      {/* <Button variant='primary' onClick={() => setModalShow(true)}>
-        Launch modal with grid
-      </Button> */}
+//   // return (
+//   //   <React.Fragment>
+//   {
+//     /* <Button variant='primary' onClick={() => setModalShow(true)}>
+//         Launch modal with grid
+//       </Button> */
+//   }
 
-      <MydModalWithGrid show={openProfile} />
-    </React.Fragment>
-  );
-};
+//   {
+//     /* <MydModalWithGrid show={openProfile} />
+//     </React.Fragment>
+//   ); */
+//   }
+// };
 
-export default Profile;
+// export default Profile;
